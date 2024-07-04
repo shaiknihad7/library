46517887edc5e3a36af3e26a3fe34a0e6cb15c73
shaiknihad7 <shaiknihad7@gmail.com>
2024-07-04 16:54:16 +0530

    Update cicd.groovy

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -11,4 +11,9 @@ def deployTomcat(jobname,ip,context)
 {
     sh "scp /var/lib/jenkins/workspace/${jobname}/webapp/target/webapp.war ubuntu@${ip}:/var/lib/tomcat9/webapps/${context}.war"
 }
+def runSelenium(jobname)
+{
+   sh 'java -jar /var/lib/jenkins/workspace/${jobname}/testing.jar'
+}
+
 
